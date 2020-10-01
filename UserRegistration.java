fe74b0a1e185f7c5c7092c40c74864193b408193
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-10-01 11:41:32 +0530

    added:User Registration System Last name is added

--- UserRegistration.java
+++ UserRegistration.java
@@ -7,11 +7,10 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 public static void main(String[] args) {
        Scanner sc=new Scanner(System.in);
-       System.out.println("Enter firstname");
+       System.out.println("Enter Lastname");
        String name=sc.next();
        System.out.println(name);
-       String pattern="^[A-Z]{1}[a-z]{2,}$" ; 
-      
+       String pattern="^[A-Z]{1}[a-z]{2,}$" ;
        Pattern namePattern=Pattern.compile(pattern);
        Matcher m=namePattern.matcher(name);
        System.out.println("Match -"+m.matches());
